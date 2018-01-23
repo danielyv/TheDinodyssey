5c8379fcb5373343537f92088081389e958f759b
Unknown <laurancy.dorian@gmail.com>
2018-01-23 13:49:13 +0100

    Vaisseau + background

---
+++ Assets/Minijeu2/Spaceship.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour {
+	
+	public float moveSpeed = 10f;
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow))
+			transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+		if(Input.GetKey(KeyCode.DownArrow))
+			transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+	}
+}
