af7a62d286f24c1144bedb9cec760f8bcf74cab2
Unknown <aloys.vernhet@orange.fr>
2018-01-23 17:47:48 +0100

    Shoot

--- Assets/Minijeu2/ShootScript.cs
+++ Assets/Minijeu2/ShootScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ShootScript : MonoBehaviour {
 
 	public int speed=8;
-	public Vector2Int direction;
+	public Vector2 direction;
 	public int orientation;
 
 	public void Go () {
@@ -19,12 +19,20 @@ public class ShootScript : MonoBehaviour {
 			direction = new Vector2 (0,speed);
 		}
 		else if(orientation == 135){
-			direction = new Vector2 (speed/2,speed/2);
+			direction = new Vector2 (-(speed/2 + speed/4),(speed/2 + speed/4));
 		}
 		else if(orientation == 180){
-			direction = new Vector2 (speed,speed);
+			direction = new Vector2 (-speed,0);
 		}
-
+		else if(orientation == 225){
+			direction = new Vector2 (-(speed/2 + speed/4),-(speed/2 + speed/4));
+		}
+		else if(orientation == 270){
+			direction = new Vector2 (0,-speed);
+		}
+		else if(orientation == 315){
+			direction = new Vector2 ((speed/2 + speed/4),-(speed/2 + speed/4));
+		}
 		Rigidbody2D r2d = GetComponent<Rigidbody2D>() as Rigidbody2D;
 
 		r2d.velocity = direction;
