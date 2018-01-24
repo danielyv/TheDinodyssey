d21f1eaf2b6169b386c8c8c9b716be4a3f0f324b
sibylleginger <rouxsibylle@gmail.com>
2018-01-24 05:43:58 +0100

    3

---
+++ Assets/Scripts/Start.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start : MonoBehaviour {
+
+	public void PlayGame()
+    {
+        SceneManager.LoadScene("MiniJeu1");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
